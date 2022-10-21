0180d9e3974128b7923e91debcb13a8ab87af976
Ricardo Rico <programador5781@gmail.com>
2022-10-20 20:05:21 -0500

    refactor card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CardTemp from './CardTemp';
 
 export default function Card({max, min, name, img, onClose}) {
   // acá va tu código
@@ -9,15 +10,10 @@ export default function Card({max, min, name, img, onClose}) {
     <div>
       <button onClick= {handleonClose}>X</button>
       <span>{name}</span>
-      <div>
-        <label>Min</label>
-        <span>{min}</span>
-      </div>
-      <div>
-        <label>Max</label>
-        <span>{max}</span>
-      </div>
+      <CardTemp label="Min" value={min}/>
+      <CardTemp label="Max" value={max}/>
       <img src={` http://openweathermap.org/img/wn/${img}@2x.png`} alt="icono del clima"/>
     </div>
   );
 }
+
---
+++ src/components/CardTemp.jsx
@@ -0,0 +1,11 @@
+function CardTemp({label, value}) {
+    return (
+    <div>
+      <label>{label}</label>
+      <span>{value}</span>
+    </div>
+    );
+}
+
+export default CardTemp;
+
