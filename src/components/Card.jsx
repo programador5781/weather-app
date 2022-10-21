7979128ffdb2218ae4652cda6e451eed3ebd63b4
Ricardo Rico <programador5781@gmail.com>
2022-10-20 20:41:46 -0500

    add validation in props of card components

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CardTemp from './CardTemp';
+import PropTypes from 'prop-types';
 
 export default function Card({max, min, name, img, onClose}) {
   // acá va tu código
@@ -15,5 +16,17 @@ export default function Card({max, min, name, img, onClose}) {
       <img src={` http://openweathermap.org/img/wn/${img}@2x.png`} alt="icono del clima"/>
     </div>
   );
+}
+
+// De la siguiente manera hacemos validación de datos de que tipo de dato es la propiedad.
+// Para esto instalamos con npm i prop-types y luego importamos import PropTypes from 'prop-types';
+// Los errores los podemos ver en consola.
+
+Card.propTypes = {
+  max: PropTypes.number,
+  min: PropTypes.number,
+  name: PropTypes.string,
+  img: PropTypes.string,
+  onClose: PropTypes.func
 }
 
